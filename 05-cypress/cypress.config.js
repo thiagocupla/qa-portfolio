05ddf89ed896786caeb22e4b7c90dbae5b955186
Thiago Tiburcio <thiagocupla@hotmail.com>
2026-08-14 20:30:12 -0300

    feat: adiciona configuracao do mochawesome para relatorios html

--- 05-cypress/cypress.config.js
+++ 05-cypress/cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: true,
+    html: true,
+    json: false,
   },
 });
