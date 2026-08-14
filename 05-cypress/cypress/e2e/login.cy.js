896aa7291150a498c3cbc7eb4b4d0238f89645a1
Thiago Tiburcio <thiagocupla@hotmail.com>
2026-08-14 19:41:08 -0300

    test: adiciona cenarios ct-002 e ct-003 de login no cypress

--- 05-cypress/cypress/e2e/login.cy.js
+++ 05-cypress/cypress/e2e/login.cy.js
@@ -6,16 +6,34 @@ describe('Automação de Testes - Tela de Login (SauceDemo)', () => {
   })
 
   it('CT-001: Deve realizar login com sucesso usando credenciais válidas', () => {
-    // Preenche os campos de credenciais
     cy.get('#user-name').type('standard_user')
     cy.get('#password').type('secret_sauce')
-
-    // Clica no botão de login
     cy.get('#login-button').click()
 
-    // Validações (Asserções)
     cy.url().should('include', '/inventory.html')
     cy.get('.title').should('have.text', 'Products')
+  })
+
+  it('CT-002: Deve exibir mensagem de erro ao tentar logar com usuário bloqueado', () => {
+    cy.get('#user-name').type('locked_out_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+
+    // Valida a mensagem de erro na tela
+    cy.get('[data-test="error"]')
+      .should('be.visible')
+      .and('contain', 'Epic sadface: Sorry, this user has been locked out.')
+  })
+
+  it('CT-003: Deve exibir mensagem de erro ao digitar senha inválida', () => {
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('senha_incorreta')
+    cy.get('#login-button').click()
+
+    // Valida se a mensagem de erro genérica aparece
+    cy.get('[data-test="error"]')
+      .should('be.visible')
+      .and('contain', 'Username and password do not match')
   })
 
 })
