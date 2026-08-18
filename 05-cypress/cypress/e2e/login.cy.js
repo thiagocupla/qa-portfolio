e5fb1d6ac5be232888f776ad4877998f127c57b6
Thiago Tiburcio <thiagocupla@hotmail.com>
2026-08-18 17:23:26 -0300

    refactor: implementando custom command para fluxo de login

--- 05-cypress/cypress/e2e/login.cy.js
+++ 05-cypress/cypress/e2e/login.cy.js
@@ -1,39 +1,23 @@
 describe('Automação de Testes - Tela de Login (SauceDemo)', () => {
 
-  beforeEach(() => {
-    // Visita a página do SauceDemo antes de cada teste
-    cy.visit('https://www.saucedemo.com/')
-  })
-
   it('CT-001: Deve realizar login com sucesso usando credenciais válidas', () => {
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
-
+    // Usando o comando customizado em uma única linha
+    cy.realizarLogin('standard_user', 'secret_sauce')
+    
+    // Validação
     cy.url().should('include', '/inventory.html')
-    cy.get('.title').should('have.text', 'Products')
   })
 
   it('CT-002: Deve exibir mensagem de erro ao tentar logar com usuário bloqueado', () => {
-    cy.get('#user-name').type('locked_out_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
-
-    // Valida a mensagem de erro na tela
-    cy.get('[data-test="error"]')
-      .should('be.visible')
-      .and('contain', 'Epic sadface: Sorry, this user has been locked out.')
+    cy.realizarLogin('locked_out_user', 'secret_sauce')
+    
+    cy.get('[data-test="error"]').should('contain', 'Epic sadface: Sorry, this user has been locked out.')
   })
 
   it('CT-003: Deve exibir mensagem de erro ao digitar senha inválida', () => {
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('senha_incorreta')
-    cy.get('#login-button').click()
-
-    // Valida se a mensagem de erro genérica aparece
-    cy.get('[data-test="error"]')
-      .should('be.visible')
-      .and('contain', 'Username and password do not match')
+    cy.realizarLogin('standard_user', 'senha_errada')
+    
+    cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
   })
 
 })
--- 05-cypress/cypress/support/commands.js
+++ 05-cypress/cypress/support/commands.js
@@ -1,25 +1,6 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('realizarLogin', (usuario, senha) => {
+  cy.visit('https://www.saucedemo.com/')
+  cy.get('[data-test="username"]').type(usuario)
+  cy.get('[data-test="password"]').type(senha)
+  cy.get('[data-test="login-button"]').click()
+})
